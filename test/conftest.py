94b9448a067fce03190f996710f531a22b1b3010
Иван Насонов <108430992+nason9972@users.noreply.github.com>
2025-11-19 20:31:23 +0300

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 def config_browser():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.driver_options = webdriver.ChromeOptions()
-    #browser.config.driver_options.add_argument('--headless=new')
+    browser.config.driver_options.add_argument('--headless=new')
     browser.config.driver_options.add_argument('--window-size=1920,1080')
     browser.config.timeout = 8
     yield
